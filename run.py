dc2cc0773e100c3bdd0ffc5c8c8a6979c577c5b3
M.Lokadithya <129350478+LokadithyaM@users.noreply.github.com>
2025-01-19 12:46:29 +0530

    Update run.py

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    # Only use the Flask development server if running locally, not in production
+    app.run(host='0.0.0.0', port=5000, debug=True)
