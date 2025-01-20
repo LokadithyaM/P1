e5302538f1b63df1c419944df85b6ffbfcd96163
M.Lokadithya <129350478+LokadithyaM@users.noreply.github.com>
2025-01-20 17:50:22 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,7 @@
 from app import create_app
-from app.models import db  # Import the db from models
 
 app = create_app()
 
-# This will create the tables in the database
-with app.app_context():
-    db.create_all()  # Create all tables (this is required the first time you run the app)
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)  # This will run the Flask app in development mode
+    # Only use the Flask development server if running locally, not in production
+    app.run(host='0.0.0.0', port=5000, debug=True)
