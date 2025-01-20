f76bfab8f2b450a677a4de8c5f97b85d6e4eb768
M.Lokadithya <129350478+LokadithyaM@users.noreply.github.com>
2025-01-20 13:50:51 +0530

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -9,3 +9,11 @@ def index():
 @bp.route('/about')
 def about():
     return render_template('about.html')
+
+@bp.route('/login')
+def login():
+    return render_template('login.html')  # Render the login page
+
+@bp.route('/signup')
+def signup():
+    return render_template('signup.html')  # Render the signup page
